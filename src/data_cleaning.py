5ab768923f2d31391cc7d7179f41626d670358ac
Moritz Wilksch <moritzwilksch@gmail.com>
2022-01-07 18:13:03 +0100

    merge lines

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -23,15 +23,23 @@ def main():
         ]
         all_lines.extend(lines)
 
-    for idx, line in enumerate(all_lines):
-        if idx == len(all_lines) - 1:
-            break  # last line
+    # for idx, line in enumerate(all_lines):
+    #     if idx == len(all_lines) - 1:
+    #         break  # last line
 
-        all_lines[idx] = line + " " + all_lines[idx + 1].split()[0]
+    #     all_lines[idx] = line + " " + all_lines[idx + 1].split()[0]
+
+    # merge lines
+    n_to_merge = 3
+    merged_lines = []
+    for idx in range(0, len(all_lines) - n_to_merge, n_to_merge):
+        merged_lines.append(
+            " ".join(all_lines[idx : idx + n_to_merge])
+        )
 
     # ------------- Writing output -----------------
     with open("data/clean_lines.txt", "w") as f:
-        for line in all_lines[1:]:  # first line is credits to data source
+        for line in merged_lines[1:]:  # first line is credits to data source
             f.write(line + "\n")
 
     c.print("[bold green][INFO][/] Cleaned up data and saved to data/clean_lines.txt.")
